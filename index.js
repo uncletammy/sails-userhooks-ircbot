3892fc04b00028ad13c97883d293a111cdc3a616
uncletammy <uncletammy@gmail.com>
2014-07-21 17:24:02 -0500

    added new methods

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ module.exports = function irc(sails) {
         bots:{},
         registerBot: function(thisBot){
             var hook = this;
-            console.log('Registering bot:',thisBot.name)
             try {
-                var botChannels = _.pluck(thisBot.channels,'name');
+                var botChannels = thisBot.channels;
                 console.log(thisBot.name,' will join channels:',botChannels)
                 var newBot = new hook.irc.Client(thisBot.host, thisBot.name, {autoConnect: thisBot.autoconnect, autoRejoin: true, channels: botChannels})
             } catch (botCreateError){
@@ -25,11 +24,12 @@ module.exports = function irc(sails) {
 
             var validEvents = ['say','error','raw','channellist','registered','motd','names','names#channel','topic','join','join#channel','part','part#channel','quit','kick','kick#channel','kill','message','message#','message#channel','notice','ping','pm','ctcp','ctcp-notice','ctcp-privmsg','ctcp-version','nick','invite','+mode','-mode','whois','channellist_start','channellist_item']
 
-            if (sails.config.irc[thisBot.name] && sails.config.irc[thisBot.name].events){
-                var eventsToRegister = sails.config.irc[thisBot.name].events;
+            if (sails.config.irc.bots[thisBot.name] && sails.config.irc.bots[thisBot.name].events){
+                var eventsToRegister = sails.config.irc.bots[thisBot.name].events;
+            } else if (thisBot.events){
+                var eventsToRegister = thisBot.events;
             } else {
                 console.log('Cant find ircBot logic in "config/irc.js".  Using default event logic.')
-                var eventsToRegister = sails.config.irc['defaultBotEvents'].events;
             }
 
             var userSuppliedEventNames = _.keys(eventsToRegister);
@@ -39,27 +39,109 @@ module.exports = function irc(sails) {
                 console.log('These events may not be valid but we will register them anyway:',possiblyInvalidEvents)
 
             for (var eventName in eventsToRegister){
-                console.log('Registering',eventName,'event for',thisBot.name);
+                console.log('Registering `',eventName,'` event for',thisBot.name);
                 hook.bots[thisBot.name].addListener(eventName,eventsToRegister[eventName]);
+            }
+
+            var updateLastSpoke = function(){
+                var currentTime = new Date();
+                var botName = thisBot.name;
+                var correctBot = hook.bots[thisBot.name];
+                correctBot.lastSpoke = currentTime;
+
+                // console.log(botName,'last spoke on',currentTime)
+            };
+
+            if (thisBot.autokick){
+
+                hook.bots[thisBot.name].lastSpoke = new Date();
+
+                var doKickWatch = function(){
+                    var currentTime = new Date();
+                    var correctBot = hook.bots[thisBot.name];
+                    var botName = thisBot.name;
+                    var getLastSpoke = correctBot.lastSpoke;
+                    var timeDifference = currentTime-correctBot.lastSpoke;
+
+                    if (timeDifference >= correctBot.autokick){
+                        console.log('kicking',botName,'due to inactivity');
+                        hook.signOutBot(correctBot)
+                    }
+
+                    console.log(botName,'last spoke',timeDifference,'miliseconds ago');
+                };
+
+                hook.bots[thisBot.name].autokick = thisBot.autokick;
+                hook.bots[thisBot.name].kickWatchIntervalID = setInterval(doKickWatch,20*1000);
+                hook.bots[thisBot.name].addListener('selfMessage',updateLastSpoke);
             }
 
             return {name:thisBot.name,success:true};
         },
-        signIn: function(message){
+        spawnNewBot: function(botObject){
+            var hook = this;
+            try {
+                hook.registerBot(botObject)
+                return true;
+            } catch (botRegError){
+                console.log('Error registering bot:',botRegError);
+                return false;
+            }
+        },
+        signOutBot: function(botToDisconnect){
+            var hook = this;
+            // console.log('shutting down interval id:',botToDisconnect.kickWatchIntervalID);
+            clearInterval(botToDisconnect.kickWatchIntervalID);
+            var oldBotCount = Object.keys(hook.bots).length;
+            botToDisconnect.disconnect();
+            console.log('Deleting bot',botToDisconnect.nick)
+            delete hook.bots[botToDisconnect.nick];
+            console.log('There were',oldBotCount,'. Now there are',Object.keys(hook.bots).length);
+        },
+        signInBots: function(message){
             var hook = this;
 
             // console.log('Registering Bots with config ',config)
             var botResults = [];
-            Bot.find().populate('channels').exec(function(err,allBots){
-                if (err){
-                    console.log('There was an error getting the bot config from the DB')
-                    return err;
-                }
 
-                botResults = botResults.concat(_.each(allBots,hook.registerBot));
-                console.log('Bot Registration Results:',botResults)
+            if (sails.models['bot']){
+                Bot.find().populate('channels').exec(function(err,allModelBots){
+                    if (err){
+                        console.log('There was an error getting the bot config from the DB')
+                        return err;
+                    }
 
-            })
+                    botResults = botResults.concat(_.each(allModelBots,hook.registerBot));
+
+                })
+            } else if (sails.config.irc.bots){
+                // Check for bots in irc.js config file;
+                var allTheBots = sails.config.irc.bots;
+
+                _.each(allTheBots,function(oneBot){
+
+                    var botObject = {
+                        name: oneBot.config.name,
+                        autoconnect: oneBot.config.autoconnect,
+                        autorejoin: oneBot.config.autorejoin,
+                        host: oneBot.config.host,
+                        channels: oneBot.config.channels,
+                        events: oneBot.events,
+                        autokick: oneBot.autokick
+                    }
+
+                    console.log('Found Bot:',botObject)
+                    botResults.push(hook.registerBot(botObject));
+
+                })
+
+            } else {
+                console.log('No bots to be found')
+            }
+
+
+
+            console.log('Bot Registration Results:',botResults)
         },
         defaults: {
             irc: {
@@ -85,10 +167,10 @@ module.exports = function irc(sails) {
                 eventsToWaitFor.push('hook:orm:loaded');
             }
 
-            sails.after(eventsToWaitFor, hook.signIn);
+            sails.after(eventsToWaitFor, hook.signInBots);
 
             // You must trigger `cb` so sails can continue loading.  If you pass in an error, sails will fail to load, and display your error on the console.
             return cb();
-        },
+        }
     }
 };
